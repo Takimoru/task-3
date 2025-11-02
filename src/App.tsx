a4070bef0a08d37fa9cd457d94bc0dabfa4b4fef
unknown <nicolastz127@gmail.com>
2025-11-02 21:30:41 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DialogDemo } from "./components/Dialog";
+import Header from "./components/Header";
+
+function App() {
+  // handleDelete sekarang akan memanggil deleteTask dari context
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1> this is my app</h1>;
+}
+
+export default Header;
