dc74de5fb7931ef80b63089b1d2dd5100460d12e
unknown <nicolastz127@gmail.com>
2025-11-02 23:48:51 +0800

    fixed bug cant resolve main.tsx cause by naming project folder with forbidden symbols

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { DialogDemo } from "./components/Dialog";
 import Header from "./components/Header";
+import { DialogDemo } from "./components/Dialog";
 
 function App() {
   // handleDelete sekarang akan memanggil deleteTask dari context
   return (
     <>
       <Header></Header>
+      <DialogDemo></DialogDemo>
     </>
   );
 }
